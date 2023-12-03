32a64f9e323b271cca43a445facccaaeb12866c7
iago2900 <iago2900@gmail.com>
2023-12-03 20:44:59 +0100

    Fix secret_key issue

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Load configuration environment
+app.config.from_object("config.ProductionConfig")
+
 from app import views
--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from app import app
 
 if __name__ == "__main__":
-
-    # Load configuration environment
-    app.config.from_object("config.ProductionConfig")
     
     app.run()
