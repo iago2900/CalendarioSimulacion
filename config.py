c5547dbf4ca33bcb7836b26b88319fa3dc40b4d1
iago2900 <iago2900@gmail.com>
2023-12-04 20:24:51 +0100

    Hide secret key in .env

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class BaseConfig():
     TESTING = False
     DEBUG = False
-    SECRET_KEY = '30ce06bd5a0d6089a26e0e84e933947f'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SESSION_TYPE = 'filesystem'
     SESSION_COOKIE_SECURE = True # cookies are only send if https
 
